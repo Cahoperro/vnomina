e0e47bb72f6b2e97cc473efe837f1c23daf4026b
Cahoperro <cahoperro1@gmail.com>
2013-04-02 19:35:46 +0200

    02-04-2013

--- src/vnomina/Impresion.java
+++ src/vnomina/Impresion.java
@@ -15,20 +15,20 @@ public class Impresion {
     try {
             JTextArea texto = new JTextArea();
             texto.append(mes + " de " +Obj.anio +"\n\n");
-            texto.append("PERCEPCIONES SALARIALES: \t\tDEDUCCIONES:");
-            texto.append("Salario base: \t" + Obj.salarioBase + "\t\tC.comunes: \t" + Obj.cComunes + "\n");
-            texto.append("Horas extra: \t" + Obj.tExtra + "\t\tDesempleo: \t" + Obj.desempleo + "\n");
-            texto.append("Antiguedad: \t" + Obj.tAntiguedad + "\t\tF.profesional: \t" + Obj.fp + "\n");
-            texto.append("Plus festivos: \t" + Obj.festivos + "\t\tT.aportaciones: \t" + Obj.tAportaciones + "\n");
-            texto.append("Peligrosidad: \t" + Obj.pPeligrosidad + "\t\tIRPF: \t" + Obj.tIrpf + "\n");
-            texto.append("Plus nocturnos: \t" + Obj.nocturnos + "\t\tTotal a deducir: \t" + Obj.tDeducir + "\n");
-            texto.append("P. pagas extra: \t" + Obj.pPagasExtras + "\t\tLiquido a percibir: \t" + Obj.liquido + "\n");
-            texto.append("Plus arma: \t\n" + Obj.tArma);
-            texto.append("Radioscopia: \t\n" + (Obj.tHorasRadio + Obj.tHorasRadioB));
-            texto.append("PERCEPCIONES NO SALARIALES: \n");
-            texto.append("Plus transporte: \t\n" + Obj.pTransporte);
-            texto.append("Plus vestuario: \t\n" + Obj.pVestuario);
-            texto.append("Total devengado: \t\n" + Obj.tDevengado);
+            texto.append("PERCEPCIONES SALARIALES: \tDEDUCCIONES:\n\n");
+            texto.append("Salario base: \t" + Obj.salarioBase + "\tC.comunes: \t" + Obj.dCcomunes + "\n");
+            texto.append("Horas extra: \t" + Obj.tExtra + "\tDesempleo: \t" + Obj.desempleo + "\n");
+            texto.append("Antiguedad: \t" + Obj.tAntiguedad + "\tF.profesional: \t" + Obj.fp + "\n");
+            texto.append("Plus festivos: \t" + Obj.festivos + "\tT.aportaciones: \t" + Obj.tAportaciones + "\n");
+            texto.append("Peligrosidad: \t" + Obj.pPeligrosidad + "\tIRPF: \t" + Obj.IRPF + "\n");
+            texto.append("Nocturnos: \t" + Obj.nocturnos + "\tTotal a deducir: \t" + Obj.tDeducir + "\n");
+            texto.append("P. pagas extra: \t" + Obj.pPagasExtras + "\tLiquido a percibir: \t" + Obj.liquido + "\n");
+            texto.append("Plus arma: \t" + Obj.tArma + "\n");
+            texto.append("Radioscopia: \t" + (Obj.tHorasRadio + Obj.tHorasRadioB)+ "\n\n");
+            texto.append("PERCEPCIONES NO SALARIALES: \n\n");
+            texto.append("Plus transporte: \t" + Obj.pTransporte+ "\n");
+            texto.append("Plus vestuario: \t" + Obj.pVestuario+ "\n");
+            texto.append("Total devengado: \t" + Obj.tDevengado+ "\n");
             
             texto.print();
         } catch (PrinterException ex) {
